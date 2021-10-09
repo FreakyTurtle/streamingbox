0b7325f25c1814d93d1c980af2082182a1d18291
FreakyTurtle <jagues@gmail.com>
2021-10-09 03:28:33 +0100

    nearly there

--- init.sh
+++ init.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+cd home/pi/streamingbox
+
 echo "installing dependency..."
 sudo npm install pm2 -g
 
 echo "starting server..."
-cd ./backend
-./init.sh
+cd home/pi/streamingbox/backend
+home/pi/streamingbox/backend/init.sh
 echo "starting client gui..."
-cd ..
-cd ./client
-./init.sh
-cd ..
+cd home/pi/streamingbox/client
+home/pi/streamingbox/init.sh
+cd home/pi/streamingbox
 
 chromium-browser --kiosk https://localhost:3000
