61d0805b193568f65b6e99ce0ce785d99e99c3f9
FreakyTurtle <jagues@gmail.com>
2021-10-09 22:11:46 +0100

    cmon

--- init.sh
+++ init.sh
@@ -4,12 +4,15 @@ rm -r /home/pi/boxlogs
 echo "pulling" >> /home/pi/boxlogs
 git pull
 
-cd backend
-cd home/pi/streamingbox/init.sh
+home/pi/streamingbox/backend/init.sh
 
-cd ..
-cd client
-cd home/pi/streamingbox/init.sh
+home/pi/streamingbox/client/init.sh
+
+echo "waiting for localhost" >> /home/pi/boxlogs
+until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
+    printf '.'
+    sleep 5
+done
 
 echo "starting chromium" >> /home/pi/boxlogs
 chromium-browser --kiosk http://localhost:3000 >> /home/pi/boxlogs
