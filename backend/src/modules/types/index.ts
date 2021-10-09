68c1ac6fea308d7db8e97fb029fa8fe6801c12c2
FreakyTurtle <jagues@gmail.com>
2021-10-09 18:55:17 +0100

    nearly there

--- backend/src/modules/types/index.ts
+++
@@ -1,9 +0,0 @@
-// export interface IFileData {
-//   id: string
-//   example: string
-// }
-
-// export interface RequestDocumentsUploadBody {
-//   id: string
-//   name: string
-// }
--- backend/src/modules/vpn/index.ts
+++ backend/src/modules/vpn/index.ts
@@ -4,19 +4,27 @@ let process: ChildProcessWithoutNullStreams | null = null
 let isConnected = false
 export const connect = () => {
   return new Promise<void>((resolve, reject) => {
+    console.log("connecting", process)
+    console.log("already connecteed", (process && process.pid))
     if(process && process.pid) resolve()
     process = spawn("sudo openvpn --config /home/pi/client.ovpn")
     process.stdout.on("data", data => {
+      console.log("process data: ", data)
       if(data.includes("Initialization Sequence Completed")) resolve()
     });
     process.on('error', (error) => {
+      console.log("process error: ", error)
       reject(error)
     });
   })
 }
 
 export const disconnect = () => {
+  console.log("disconnecting")
+  console.log("disconnected already", (!process || !process.pid))
   if(!process || !process.pid) return 
+  console.log("killing process");
+  
   process.kill('SIGTERM')
   //or process.kill(job.pid, 'SIGTERM')
   process = null
