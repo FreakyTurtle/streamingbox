1415cc3e0e20b1dc346456cba75bb2bd37f93ca9
FreakyTurtle <jagues@gmail.com>
2021-10-09 19:53:59 +0100

    cmon

--- backend/src/modules/vpn/index.ts
+++ backend/src/modules/vpn/index.ts
@@ -26,16 +26,24 @@ export const connect = () => {
 }
 
 export const disconnect = () => {
-  console.log("disconnecting", process)
-  console.log("disconnected already", (!process || !process.pid))
-  if(!process || !process.pid) return 
-  console.log("killing process");
-  
-  process.kill('SIGTERM')
-  //or process.kill(job.pid, 'SIGTERM')
-  process = null
-  isConnected = false
-  return
+  return new Promise<void>((resolve, reject) => {
+    console.log("disconnecting", process)
+    console.log("disconnected already", (!process || !process.pid))
+    if(!process || !process.pid) return 
+    console.log("killing process");
+    process.kill('SIGTERM')
+    const exit_proc = spawn("sudo", ["killall", "-SIGINT", "openvpn"])
+    exit_proc.stdout.on("end", () => {
+      process = null
+      isConnected = false
+      resolve()
+    })
+    exit_proc.stdout.on("error", (error) => {
+      console.log(error)
+      reject()
+    })
+    //or process.kill(job.pid, 'SIGTERM')
+  })
 }
 
 export const isConn = () => (process && process.pid)
