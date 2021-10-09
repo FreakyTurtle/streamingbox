153d05d3a6f6734ee967c27fd736dc33d3c8edaf
FreakyTurtle <jagues@gmail.com>
2021-10-09 03:19:12 +0100

    nearly there

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "installing dependency..."
 sudo npm install pm2 -g
 
+echo "starting server..."
 cd ./backend
 ./init.sh
+echo "starting client gui..."
 cd ..
 cd ./client
 ./init.sh
